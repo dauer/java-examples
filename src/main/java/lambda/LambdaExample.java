296e561138684728d0e1540aea2082f6a5fa659a
Thomas Rasmussen <thomas@gennemtaenkt.dk>
2025-12-15 13:18:36 +0100

    Add FunctionalInterface to lambda example

--- src/main/java/lambda/LambdaExample.java
+++ src/main/java/lambda/LambdaExample.java
@@ -5,20 +5,29 @@ interface I {
     void doSomething();
 }
 
+@FunctionalInterface
+interface X {
+    void doSomethingElse(int i);
+}
+
 public class LambdaExample {
 
     public static void main(String[] args) {
         // Pre Java 8 lambdas
-        I i = new I() {
+        I i1 = new I() {
             @Override
             public void doSomething() {
                 System.out.println("I do something");
             }
         };
         // Java 8+
-        i.doSomething();
+        i1.doSomething();
+
         I i2 = () -> System.out.println("I2 do something");
         i2.doSomething();
+
+        X x2 = (int x) -> System.out.println("X2 do something " + (x * 2));
+        x2.doSomethingElse(10);
     }
 
 }
